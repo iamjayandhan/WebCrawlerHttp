9d7aed6f9bd62e747cda676f59dc975ee70ea3e1
iamjayandhan <jayandhanrajendran03@gmail.com>
2025-04-02 12:29:01 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,29 +1,72 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL){
+//here:
+//baseURL -> entry point (Like a home page for the website)
+//currentURL -> current crawling page
+//pages -> object that holds all the crawled pages so far
+
+//must to check if currentURL and baseURL are from the same domain!
+async function crawlPage(baseURL,currentURL,pages){
     console.log(`actively crawling: ${currentURL}`);
 
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    //handle external links!
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages;
+    }
+
+    //check if the page is already crawled!
+    //how many times same link is crawled! we take a count for providing a report
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if(pages[normalizedCurrentURL] > 0 ){
+
+        //if already crawled, then only increment that map count!
+        //no need to crawl again! immediately return!
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+
+    //we are going to crawl for the first time!
+    pages[normalizedCurrentURL] = 1;
+
     try{
+        //try to get the html page for parsing! (CurrentURL's Page -> HTML version)
         const resp = await fetch(currentURL);
 
         //400-499 for client & 500-599 for server error!
         if(resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
         //check for content type
         const contentType = resp.headers.get("content-type");
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
-        console.log(await resp.text()); //.text() method returns a promise! so handle it gracefully!
+        // console.log(await resp.text()); //.text() method returns a promise! so handle it gracefully!
+        const htmlBody = await resp.text();
+
+        //this nextURLs contains all the links present in that currentURL's htmlBody!
+        const nextURLs = getURLsFromHTML(htmlBody,baseURL);
+
+        //recursively iterate over all the urls present in the currentURL's htmlBody! (nextURLs)
+        for(const nextURL of nextURLs){
+            //recursively call the same function!
+            //based on urls, it will refine that pages map!
+            //if new, creates entry, if already visited, then count is incremented! 
+            pages = await crawlPage(baseURL,nextURL, pages);
+        }
     }
     catch(err){
         console.log(`error fetching data: ${err.message} at ${currentURL}`);
     }
+
+    return pages;
 }
 
 //input -> html page
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const { crawlPage } = require('./crawl.js');
 
-function main(){
-    if(process.argv.length < 3){
+async function main(){
+    if(process.argv.length < 3){ //check command line args! 
+        //by default, 2 args will be there, check if 3rd(user's website link) is present!
         console.log(`No Website provided`);
         process.exit(1);
     }
 
     if(process.argv.length > 3){
+        //check if more than one website link is given!
         console.log(`Too many command line args`);
         process.exit(1);
     }
@@ -18,7 +20,13 @@ function main(){
     //     console.log(arg); //C:\nvm4w\nodejs\node.exe, D:\Projects\WebCrawlerHttp\main.js, http://example.com
     // }
     console.log(`Starting crawl of ${baseURL}`);
-    crawlPage(baseURL);
+
+    //baseURL, currentURL, pages object!
+    const pages = await crawlPage(baseURL,baseURL, {});
+
+    for(const page of Object.entries(pages)){
+        console.log(page);
+    }
 }
 
 main()
